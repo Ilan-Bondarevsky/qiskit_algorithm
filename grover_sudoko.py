a20d0b377d544534bdb3e83250dcf79e14fb481e
Ilan Bondrevsky <bondleroot101@yahoo.com>
2023-12-19 15:07:25 +0200

    Fixed mor gate

--- grover_sudoko.py
+++ grover_sudoko.py
@@ -7,6 +7,7 @@ from quantum_operation import QuantumOperation
 from grover_cirq import diffuser
 from grover_num_list_cirq import num_oracle
 import math
+from itertools import combinations
 
 def diffrent_num(nqubits):
     num_a = QuantumRegister(nqubits, 'num_a')
@@ -17,22 +18,22 @@ def diffrent_num(nqubits):
         xor_qc = xor_gate()
         qc.add_register(xor_qc.ancillas)
         qc = qc.compose(xor_qc, [num_a[i] , num_b[i]] + list(xor_qc.ancillas))
-
+    qc.name = 'Diff_Num'
     return qc
 
 def check_under_ten_no_zero():
     nqubits = int(9).bit_length()
     qc = QuantumCircuit(nqubits)
 
-    #nand_g = nand_gate()
-    #add_anc = AncillaRegister(1)
-    #qc.add_register(add_anc)
-    #qc = qc.compose(nand_g.copy(), [1,3, add_anc])
+    nand_g = nand_gate()
+    add_anc = AncillaRegister(1)
+    qc.add_register(add_anc)
+    qc = qc.compose(nand_g.copy(), [1,3, qc.ancillas[-1]])
 
-    #qc.barrier(qc.qubits)
-    #add_anc = AncillaRegister(1)
-    #qc.add_register(add_anc)
-    #qc = qc.compose(nand_g.copy(), [2,3, add_anc])
+    qc.barrier(qc.qubits)
+    add_anc = AncillaRegister(1)
+    qc.add_register(add_anc)
+    qc = qc.compose(nand_g.copy(), [2,3, qc.ancillas[-1]])
 
     qc.barrier(qc.qubits)
 
@@ -40,11 +41,28 @@ def check_under_ten_no_zero():
     qc.add_register(add_anc)
     mor_g = mor_gate(nqubits)
     qc = qc.compose(mor_g.copy(), get_qubit_list(qc) + [qc.ancillas[-1]])
+    qc.name = "Between 1-9 (with 1 & 9)"
     return qc
 
+def sudoko_max_four(index_value_list=None):
+    iteration = QuantumCircuit(pow(2, 4))
+    comb = [list(x) for x in combinations(list(range(4)), 2)]
 
+    for c in comb:
+        diff_row = diffrent_num(2)
+        diff_col = diffrent_num(2)
+        iteration.add_register(list(diff_col.ancillas) + list(diff_row.ancillas))
+        
+
+    qc = QuantumCircuit(iteration.qubits)
+    prep = prep_search_qubits(len(qc.qubits) , index_value_list)
+    qc = qc.compose(prep, get_qubit_list(qc))
+    return qc
+    
+
+print(sudoko_max_four())
 ###
-qc_oracle = check_under_ten_no_zero()
+qc_oracle = diffrent_num(3)
 
 iteration_qc = QuantumCircuit(qc_oracle.qubits)
 
@@ -86,7 +104,7 @@ run_qc = qc
 result = execute(run_qc, backend = Aer.get_backend('aer_simulator'),shots=1024).result()
 print(result.get_counts())
 
-
+print([i for i in result.get_counts() if result.get_counts()[i] > 50])
 print(sum([val for val in result.get_counts().values()]))
 #print(diff.draw())
 #qc.add_register(diff.ancillas)
