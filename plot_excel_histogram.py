18fdd139e32cdfab870db789f315614dbc7d1c91
Ilan Bondarevsky <worldofwarcraft.bond@yahoo.com>
2024-09-05 21:54:40 +0300

    .

--- plot_excel_histogram.py
+++ plot_excel_histogram.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plot_histogram(data, field, *, bin_width, min_value=None, max_value=None, vertical_line=None):
+def plot_histogram(data, field, *, bin_width, min_value=None, max_value=None, vertical_line=None, additional_name_text : str = ''):
     time_taken = data[field]
 
     if min_value is not None:
@@ -15,7 +15,7 @@ def plot_histogram(data, field, *, bin_width, min_value=None, max_value=None, ve
     if vertical_line is not None:
         plt.axvline(vertical_line, color='red', linestyle='--', linewidth=2, label=f'Aer {field}: {vertical_line}')
     plt.hist(filtered_data, bins=int((filtered_data.max() - filtered_data.min()) / bin_width), edgecolor='black')
-    plt.title('Histogram of Result Time Taken')
+    plt.title(f'{additional_name_text} Histogram of Result Time Taken')
     plt.xlabel('Result Time Taken')
     plt.ylabel('Frequency')
     plt.grid(True)
@@ -23,9 +23,9 @@ def plot_histogram(data, field, *, bin_width, min_value=None, max_value=None, ve
         plt.legend()
     plt.show()
 
-file_path = r'D:\myProjects\AfekaCodeProjects\codeProjects\FinalProject_qiskit\qiskit_algorithm\logs\add_mod_11_results_2000_plus_48_2024_08_20_18_18_10.csv'
+file_path = r'C:\Users\User\Downloads\x\outputfile\grover_circuit_A_6_qubits.csv'
 data = pd.read_csv(file_path)
 data.drop(data[data['optimization_level']==3].index, inplace=True)
 
-plot_histogram(data, "result_time_taken", bin_width=10, max_value=1000, vertical_line=0.011923)
-plot_histogram(data, "transpiled_qc_depth", bin_width=10, vertical_line=175)
+plot_histogram(data, "result_time_taken", bin_width=0.01, vertical_line=0.0048742, additional_name_text = "Grover 6Q Search 6")
+plot_histogram(data, "transpiled_qc_depth", bin_width=10, vertical_line=1934, additional_name_text = "Grover 6Q Search 6")
